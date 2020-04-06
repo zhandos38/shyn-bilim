734d97fbde3936e07b7b61f7a4212c2f40a9d2e2
jake47 <zhandos38@gmail.com>
2020-04-06 17:11:50 +0600

    Bootstrap 4 js configured

--- frontend/assets/AppAsset.php
+++ frontend/assets/AppAsset.php
@@ -19,5 +19,6 @@ class AppAsset extends AssetBundle
     public $depends = [
         'yii\web\YiiAsset',
         'yii\bootstrap4\BootstrapAsset',
+        'yii\bootstrap4\BootstrapPluginAsset',
     ];
 }
