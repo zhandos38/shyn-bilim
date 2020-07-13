92f698c2dbbde51a963522451edd8706389f4f99
Zhandos <324614jan>
2020-07-13 15:20:03 +0600

    Article payment fixes

--- frontend/controllers/ArticleController.php
+++ frontend/controllers/ArticleController.php
@@ -7,7 +7,9 @@ namespace frontend\controllers;
 use common\models\Article;
 use common\models\Subject;
 use frontend\models\ArticleSearch;
+use Paybox\Pay\Facade as Paybox;
 use Yii;
+use yii\db\Exception;
 use yii\helpers\VarDumper;
 use yii\web\Controller;
 use yii\web\UploadedFile;
@@ -16,6 +18,7 @@ class ArticleController extends Controller
 {
     public function actionIndex()
     {
+        VarDumper::dump(Yii::$app->request->post(),10,1); die;
         $subjects = Subject::find()->all();
 
         return $this->render('index', [
@@ -38,17 +41,39 @@ class ArticleController extends Controller
     {
         $model = new Article();
 
+        $paybox = new Paybox();
+
+        $paybox->merchant->id = 530827;
+        $paybox->merchant->secretKey = 'oqyhxvX5lnzTFZTd';
+        $paybox->order->id = $model->id;
+        $paybox->order->amount = 100;
+        $paybox->order->description = 'test order';
+
+        if($paybox->init()) {
+            return $this->redirect($paybox->redirectUrl);
+        }
+
         if ($model->load(Yii::$app->request->post())) {
-            $model->fileTemp = UploadedFile::getInstance($model, 'fileTemp');
-            $model->created_at = time();
 
-            if ($model->fileTemp) {
-                $model->file = $model->fileTemp->baseName . '.' . $model->fileTemp->extension;
-            }
+            $transaction = Yii::$app->db->beginTransaction();
+            try {
+                $model->fileTemp = UploadedFile::getInstance($model, 'fileTemp');
+                $model->created_at = time();
 
-            if ($model->save() && $model->upload()) {
-                Yii::$app->session->setFlash('success', Yii::t('app', 'Ваш материал успешно опубликован'));
-                return $this->redirect(['index']);
+                if ($model->fileTemp) {
+                    $model->file = $model->fileTemp->baseName . '.' . $model->fileTemp->extension;
+                }
+
+                if ($model->save() && $model->upload()) {
+
+                    $transaction->commit();
+
+                    Yii::$app->session->setFlash('success', Yii::t('app', 'Ваш материал успешно опубликован'));
+                    return $this->redirect(['index']);
+                }
+            } catch (Exception $e) {
+                $transaction->rollBack();
+                throw new Exception($e->getMessage());
             }
         }
 
