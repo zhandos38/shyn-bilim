211c72015f88718f8c225a3b44e9bcbc3b787a3e
jake47 <zhandos38@gmail.com>
2020-08-04 00:27:03 +0600

    Cert fixes

---
+++ frontend/views/article/_cert.php
@@ -0,0 +1,11 @@
+<?php
+
+/* @var $user \common\models\User */
+?>
+<div>
+    <div class="cert-page" style="background-image: url('/img/material-cert.jpg'); background-size: cover; background-repeat: no-repeat; width: 1400px; height: 1200px;">
+        <div id="cert-name" style="padding-top: 410px; padding-left: 400px; width: 700px; text-align: center; font-size: 42px; font-family: sans-serif; font-weight: bold; color: #000000;"><?= 'Райхан Саматқызы' ?></div>
+        <div id="cert-school" style="padding-top: 38px; padding-left: 720px; font-size: 28px; font-family: sans-serif; color: #000000; height: 100px;"><?= 'Алтын белгі' ?></div>
+        <div id="cert-number" style="padding-top: 260px; padding-left: 720px; font-size: 22px; font-family: sans-serif; color: #000000;"><?= $number ?></div>
+    </div>
+</div>
