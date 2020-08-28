4efebc52b9394094be8d857c3ac0d9dff671338b
jake47 <zhandos38@gmail.com>
2020-08-28 14:20:12 +0600

    Some fixes

--- frontend/controllers/OlympiadController.php
+++ frontend/controllers/OlympiadController.php
@@ -30,6 +30,11 @@ class OlympiadController extends Controller
 
     public function actionList($type)
     {
+        if (!$type) {
+            Yii::$app->session->setFlash('error', Yii::t('app', 'Данная олимпиада еще не доступна'));
+            return $this->redirect(['index']);
+        }
+
         $dataProvider = new ActiveDataProvider([
             'query' => Subject::find()->andWhere(['type' => $type]),
             'sort' => [
