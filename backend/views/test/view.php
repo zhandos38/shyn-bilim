57bf170a7ef815b0711bd4c4f503b17eba647123
zhandos38 <zhandos38@gmail.com>
2020-10-03 16:17:22 +0600

    Olympiad admin fixes

--- backend/views/test/view.php
+++ backend/views/test/view.php
@@ -43,7 +43,7 @@ $this->params['breadcrumbs'][] = $this->title;
             [
                 'label' => 'Всего вопросов',
                 'value' => function(Test $model) {
-                    return count($model->questions);
+                    return count($model->questionsTotal);
                 }
             ]
         ],
