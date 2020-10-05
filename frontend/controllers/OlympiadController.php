8cf12811cf70d3e804c5181e959cfc4eebeefd3d
jake47 <zhandos38@gmail.com>
2020-10-05 09:02:59 +0600

    Olympiad fixes

--- frontend/controllers/OlympiadController.php
+++ frontend/controllers/OlympiadController.php
@@ -29,15 +29,6 @@ class OlympiadController extends Controller
     public function behaviors()
     {
         return [
-            'access' => [
-                'class' => AccessControl::className(),
-                'rules' => [
-                    [
-                        'allow' => true,
-                        'roles' => ['@'],
-                    ],
-                ],
-            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
