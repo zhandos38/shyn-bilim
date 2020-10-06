fa4e888caa2614e9f8ec0d0559cfaf86642fb7e3
zhandos38 <zhandos38@gmail.com>
2020-10-06 13:29:17 +0600

    Video link olympiad

--- backend/views/test-assignment/index.php
+++ backend/views/test-assignment/index.php
@@ -30,7 +30,7 @@ $this->params['breadcrumbs'][] = $this->title;
         'columns' => [
             ['class' => 'yii\grid\SerialColumn'],
 
-//            'id',
+            'id',
             [
                 'attribute' => 'test_id',
                 'value' => function(TestAssignment $model) {
