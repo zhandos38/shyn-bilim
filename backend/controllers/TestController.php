5e2c7884f2969aaf9cd732e192493b948b42ade7
zhandos38 <zhandos38@gmail.com>
2020-10-06 11:57:36 +0600

    Video link olympiad

--- backend/controllers/TestController.php
+++ backend/controllers/TestController.php
@@ -5,6 +5,7 @@ namespace backend\controllers;
 use Yii;
 use common\models\Test;
 use backend\models\TestSearch;
+use yii\filters\AccessControl;
 use yii\web\Controller;
 use yii\web\NotFoundHttpException;
 use yii\filters\VerbFilter;
@@ -20,6 +21,15 @@ class TestController extends Controller
     public function behaviors()
     {
         return [
+            'access' => [
+                'class' => AccessControl::className(),
+                'rules' => [
+                    [
+                        'allow' => true,
+                        'roles' => ['admin']
+                    ]
+                ],
+            ],
             'verbs' => [
                 'class' => VerbFilter::className(),
                 'actions' => [
