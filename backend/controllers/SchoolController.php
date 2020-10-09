6b3e4e1e3274af293471cabfc0f297f938d43457
zhandos38 <zhandos38@gmail.com>
2020-10-09 10:37:50 +0600

    fixes

--- backend/controllers/SchoolController.php
+++ backend/controllers/SchoolController.php
@@ -67,7 +67,7 @@ class SchoolController extends Controller
         $model = new School();
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         }
 
         return $this->render('create', [
@@ -87,7 +87,7 @@ class SchoolController extends Controller
         $model = $this->findModel($id);
 
         if ($model->load(Yii::$app->request->post()) && $model->save()) {
-            return $this->redirect(['view', 'id' => $model->id]);
+            return $this->redirect(['index']);
         }
 
         return $this->render('update', [
