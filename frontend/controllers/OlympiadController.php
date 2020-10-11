36ee51e675fc42a0df9f6ea56a04b1aa4cab7f3d
jake47 <zhandos38@gmail.com>
2020-10-11 15:27:16 +0600

    Olympiad fixes

--- frontend/controllers/OlympiadController.php
+++ frontend/controllers/OlympiadController.php
@@ -83,7 +83,7 @@ class OlympiadController extends Controller
 
         if ($model->load(\Yii::$app->request->post()) && $model->validate()) {
             $test = Test::findOne(['subject_id' => $model->subject_id, 'grade' => $model->grade, 'lang' => $model->lang]);
-            if (!$test) {
+            if (!$test || ($test->id === 61 || $test->id === 62)) {
                 Yii::$app->session->setFlash('error', Yii::t('app', 'Тест не найден'));
 
                 return $this->render('assignment', [
