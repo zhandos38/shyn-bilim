6c6c1a1f42e09b721247a1739c746c27fd8965f9
jake47 <zhandos38@gmail.com>
2020-12-13 20:00:35 +0600

    Olympiad assignemnt fixes

--- frontend/views/olympiad/index.php
+++ frontend/views/olympiad/index.php
@@ -22,7 +22,7 @@ $this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['olympiad/i
                 </a>
                 <div class="d-flex">
                     <a class="btn btn-info w-50 rounded-0" href="<?= $olympiad->getFile() ?>"><i class="fa fa-download"></i> <?= Yii::t('app', 'Приложение') ?></a>
-                    <a class="btn btn-success w-50 rounded-0" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>"><i class="fa fa-sign-in"></i> <?= Yii::t('app', 'Участвовать') ?></a>
+                    <a class="btn btn-success w-50 rounded-0" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>"><i class="fa fa-sign-in"></i> <?= Yii::t('app', 'Участие') ?></a>
                 </div>
             </li>
         <?php endforeach; ?>
