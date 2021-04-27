72563f04f150762ff2fafad2d09e3e89d1e4843e
zhandos38 <zhandos38@gmail.com>
2021-04-27 10:06:26 +0600

    Questions fixes

--- frontend/views/olympiad/_cert-thank.php
+++ frontend/views/olympiad/_cert-thank.php
@@ -14,7 +14,7 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $t
         </div>
         <div id="footer" style="padding-top: 205px; padding-left: 25px; font-size: 16px; color: #454545; font-family: 'Times New Roman'">
             <div id="cert-qrcode" style="padding-top: 5px; width: 160px; font-size: 22px; font-weight: bold;"><img src="<?= $qrCode->writeDataUri() ?>"></div>
-            <div id="cert-number" style="padding-top: 5px">Тіркеу нөмері №<?= $testAssignment->id ?></div>
+            <div id="cert-number" style="padding-top: 5px">Тіркеу №<?= $testAssignment->id ?></div>
             <div id="cert-date" style="padding-top: 5px;"><?= date('d.m.Y') ?> жыл</div>
         </div>
     </div>
