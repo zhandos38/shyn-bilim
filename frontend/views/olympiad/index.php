5c086aa0045a13433eef212304aea30c128a32cc
Zhandos <zhandos38@gmail.com>
2021-09-25 18:03:27 +0600

    fix: olympiad list

--- frontend/views/olympiad/index.php
+++ frontend/views/olympiad/index.php
@@ -9,22 +9,24 @@ $this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['olympiad/i
 ?>
 <div class="olympiad-index" style="padding-bottom: 80px">
     <h1><?= $this->title ?></h1>
-    <ul class="card-list">
+    <div class="row">
         <?php /** @var \common\models\Olympiad $olympiad */
         foreach ($olympiads as $olympiad): ?>
-            <li class="card">
-                <a class="card-image" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>" style="background-image: url(<?= htmlspecialchars($olympiad->getImage()) ?>);" data-image-full="<?= $olympiad->getImage() ?>">
-                    <img src="<?= $olympiad->getImage() ?>" alt="Olympiad" />
-                </a>
-                <a class="card-description" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>">
-                    <h6><?= $olympiad->name ?></h6>
-                    <p><?= $olympiad->getType() ?></p>
-                </a>
-                <div class="d-flex">
-                    <a class="btn btn-info w-50 rounded-0" href="<?= $olympiad->getFile() ?>"><i class="fa fa-download"></i> <?= Yii::t('app', 'Положение') ?></a>
-                    <a class="btn btn-success w-50 rounded-0" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>"><i class="fa fa-sign-in"></i> <?= Yii::t('app', 'Участие') ?></a>
+            <div class="col-md-4">
+                <div class="card">
+                    <a class="card-image" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>" style="background-image: url(<?= htmlspecialchars($olympiad->getImage()) ?>);" data-image-full="<?= $olympiad->getImage() ?>">
+                        <img src="<?= $olympiad->getImage() ?>" alt="Olympiad" />
+                    </a>
+                    <a class="card-description text-center" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>">
+                        <h6><?= $olympiad->name ?></h6>
+                        <p><?= $olympiad->getType() ?></p>
+                    </a>
+                    <div class="d-flex">
+                        <a class="btn btn-info w-50 rounded-0" href="<?= $olympiad->getFile() ?>"><i class="fa fa-download"></i> <?= Yii::t('app', 'Положение') ?></a>
+                        <a class="btn btn-success w-50 rounded-0" href="<?= Url::to(['olympiad/view', 'id' => $olympiad->id]) ?>"><i class="fa fa-sign-in"></i> <?= Yii::t('app', 'Участие') ?></a>
+                    </div>
                 </div>
-            </li>
+            </div>
         <?php endforeach; ?>
-    </ul>
+    </div>
 </div>
