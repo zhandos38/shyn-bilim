5cf8e7c793ab44a10b005f018a746636f7a50c40
Zhandos <zhandos38@gmail.com>
2021-10-26 14:45:22 +0600

    fix: marathon

--- frontend/views/marathon/book.php
+++ frontend/views/marathon/book.php
@@ -21,6 +21,9 @@ $this->params['heroDescription'] = 'КАНИКУЛДА КІТАП ОҚИМЫЗ';
                 <div class="post-details padding-30px-all xl-padding-25px-lr">
                     <a href="/file/marathon/2.1.pdf" class="text-extra-dark-gray font-weight-500 alt-font d-block">ТАҢДАУЛЫ ЕРТЕГІЛЕР</a>
                     <small>Ханс Кристиан Андерсен</small>
+                    <div class="mt-3">
+                        <a class="btn btn-primary" href="/file/marathon/2.1.pdf"><i class="fa fa-download"></i> <?= Yii::t('app', 'Скачать') ?></a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -36,6 +39,9 @@ $this->params['heroDescription'] = 'КАНИКУЛДА КІТАП ОҚИМЫЗ';
                 <div class="post-details padding-30px-all xl-padding-25px-lr">
                     <a href="/file/marathon/2.2.pdf" class="text-extra-dark-gray font-weight-500 alt-font d-block">ЫБЫРАЙ АЛТЫНСАРИННІҢ ӨЛЕҢДЕРІ МЕН ӘҢГІМЕЛЕРІ</a>
                     <small>Ыбырай Алтынсарин</small>
+                    <div class="mt-3">
+                        <a class="btn btn-primary" href="/file/marathon/2.2.pdf"><i class="fa fa-download"></i> <?= Yii::t('app', 'Скачать') ?></a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -52,6 +58,9 @@ $this->params['heroDescription'] = 'КАНИКУЛДА КІТАП ОҚИМЫЗ';
                 <div class="post-details padding-30px-all xl-padding-25px-lr">
                     <a href="/file/marathon/5.pdf" class="text-extra-dark-gray font-weight-500 alt-font d-block">МЕНІҢ АТЫМ ҚОЖА</a>
                     <small>Бердібек Соқпақбаев</small>
+                    <div class="mt-3">
+                        <a class="btn btn-primary" href="/file/marathon/5.pdf"><i class="fa fa-download"></i> <?= Yii::t('app', 'Скачать') ?></a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -68,6 +77,9 @@ $this->params['heroDescription'] = 'КАНИКУЛДА КІТАП ОҚИМЫЗ';
                 <div class="post-details padding-30px-all xl-padding-25px-lr">
                     <a href="/file/marathon/7.pdf" class="text-extra-dark-gray font-weight-500 alt-font d-block">Қызыл жебе</a>
                     <small>Шерхан Мұртаза</small>
+                    <div class="mt-3">
+                        <a class="btn btn-primary" href="/file/marathon/7.pdf"><i class="fa fa-download"></i> <?= Yii::t('app', 'Скачать') ?></a>
+                    </div>
                 </div>
             </div>
         </div>
@@ -84,6 +96,9 @@ $this->params['heroDescription'] = 'КАНИКУЛДА КІТАП ОҚИМЫЗ';
                 <div class="post-details padding-30px-all xl-padding-25px-lr">
                     <a href="/file/marathon/9.pdf" class="text-extra-dark-gray font-weight-500 alt-font d-block">Қанмен жазылған кітап</a>
                     <small>Бауыржан Момышұлы</small>
+                    <div class="mt-3">
+                        <a class="btn btn-primary" href="/file/marathon/9.pdf"><i class="fa fa-download"></i> <?= Yii::t('app', 'Скачать') ?></a>
+                    </div>
                 </div>
             </div>
         </div>
