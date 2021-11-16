a4aea4f5726ad336105eeeee315aaca9cbc689d6
zhandos38 <zhandos38@gmail.com>
2021-11-16 23:41:04 +0600

    Olympiad fixes

---
+++ frontend/views/olympiad/check-cert-thank-parent.php
@@ -0,0 +1,29 @@
+<?php
+
+use frontend\models\CheckAssignmentForm;
+use yii\bootstrap4\ActiveForm;
+use yii\bootstrap4\Html;
+use yii\web\View;
+
+/* @var $this View */
+/* @var $model CheckAssignmentForm */
+
+$this->title = Yii::t('app', 'Получить благодарственное письмо');
+$this->params['breadcrumbs'][] = ['label' => Yii::t('app', 'Вопросы и ответы'), 'url' => ['site/questions']];
+$this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => '#'];
+?>
+<h1 class="mb-4">
+    <?= $this->title ?>
+</h1>
+
+<?php $form = ActiveForm::begin() ?>
+
+<div class="row">
+    <div class="col-md-4">
+        <?= $form->field($model, 'iin') ?>
+        <?= Html::submitButton(Yii::t('app', 'Отправить'), ['class' => 'btn btn-primary']) ?>
+    </div>
+</div>
+
+<?php ActiveForm::end() ?>
+
