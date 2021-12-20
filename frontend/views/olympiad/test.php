85b0affce167de70ae25409e918fdd910ea370c0
Jake <zhandos38@gmail.com>
2021-12-20 10:14:26 +0600

    fixes

--- frontend/views/olympiad/test.php
+++ frontend/views/olympiad/test.php
@@ -38,12 +38,12 @@ $this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => ['subject/te
             <div v-else>
                 <div class="questions-correct-count"><?= Yii::t('app', 'Вы набрали') ?>: {{ correctAnswerCount }}</div>
                 <div>
-                    <a class="btn btn-success" :class="!isSent ? 'disabled-link' : ''" href="<?= \yii\helpers\Url::to(['/olympiad/get-cert', 'id' => $assignment_id]) ?>">
+                    <a class="btn btn-success" :class="!isSent ? 'disabled-link' : ''" href="<?= \yii\helpers\Url::to(['/olympiad/get-cert', 'id' => $assignment_id]) ?>" download>
                         <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" v-if="!isSent"></span>
                         <?= Yii::t('app', 'Получить сертификат/диплом') ?>
                     </a>
                     <br><br>
-                    <a class="btn btn-success" :class="!isSent ? 'disabled-link' : ''" href="<?= \yii\helpers\Url::to(['/olympiad/get-cert-thank-leader', 'id' => $assignment_id]) ?>">
+                    <a class="btn btn-success" :class="!isSent ? 'disabled-link' : ''" href="<?= \yii\helpers\Url::to(['/olympiad/get-cert-thank-leader', 'id' => $assignment_id]) ?>" download>
                         <span class="spinner-border spinner-border-sm" role="status" aria-hidden="true" v-if="!isSent"></span>
                         <?= Yii::t('app', 'Получить благодарственное письмо для руководителя') ?>
                     </a>
