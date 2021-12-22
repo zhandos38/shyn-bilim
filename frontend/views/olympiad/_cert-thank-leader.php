bf43dac15f81e6e71e04ef11bafb9ac567caed98
Jake <zhandos38@gmail.com>
2021-12-22 16:23:00 +0600

    xaxa

--- frontend/views/olympiad/_cert-thank-leader.php
+++ frontend/views/olympiad/_cert-thank-leader.php
@@ -15,7 +15,7 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert-thank-leader
                     <?= $testAssignment->leader_name ?>
                 </div>
             </div>
-            <div id="footer" style="text-align: left; padding-left: 20px; padding-top: 320px; width: 160px; font-size: 16px; color: #454545; font-family: 'Times New Roman'">
+            <div id="footer" style="text-align: left; padding-left: 20px; padding-top: 316px; width: 160px; font-size: 16px; color: #454545; font-family: 'Times New Roman'">
                 <div id="cert-qrcode" style="width: 160px; font-size: 22px; padding-left: -52px; font-weight: bold;"><img src="<?= $qrCode->writeDataUri() ?>"></div>
                 <div id="cert-number" style="padding-top: 5px"> №<?= $testAssignment->id ?></div>
                 <div id="cert-date" style="padding-top: 0;"><?= date('d.m.Y') ?> жыл</div>
