670c6710484c6345511024b296d08cf49412e689
Zhandos <zhandos38@gmail.com>
2022-02-12 19:19:18 +0600

    fix: olympiad fixes

--- frontend/views/olympiad/_cert-thank-leader.php
+++ frontend/views/olympiad/_cert-thank-leader.php
@@ -18,7 +18,7 @@ if ($testAssignment->grade >= 5 && $testAssignment->grade <= 6) {
     <div class="cert-page" style="background-image: url(<?= $fileName ?>); text-align: center; background-size: cover; background-repeat: no-repeat; font-family: 'Times New Roman'; height: 800px">
         <div style="padding-left: 210px; padding-top: 200px">
             <div>
-                <div style="font-size: 14px; width: 440px; padding-top: 20px; padding-left: 220px; font-weight: bold">
+                <div style="font-size: 14px; width: 440px; padding-top: 0; padding-left: 220px; font-weight: bold">
                     <?php
                     if ($testAssignment->school !== null) {
                         if ($testAssignment->school->city_id === 1 || $testAssignment->school->city_id === 2 || $testAssignment->school->city_id === 3) {
@@ -27,6 +27,7 @@ if ($testAssignment->grade >= 5 && $testAssignment->grade <= 6) {
                             echo $testAssignment->school->city->region->name . ', ' . $testAssignment->school->city->name;
                         }
                     } ?>
+                    <br>
                     <?= $testAssignment->school->name ?>
                 </div>
                 <div style="height: 162px; <?= $testAssignment->grade >= 5 ? 'padding-bottom: 35px' : '' ?>">
@@ -52,7 +53,7 @@ if ($testAssignment->grade >= 5 && $testAssignment->grade <= 6) {
             <div id="footer" style="text-align: left; padding-left: 50px; width: 160px; font-size: 16px; color: #454545; font-family: 'Times New Roman'; <?= $testAssignment->grade >= 5 ? 'padding-top: -58px' : 'padding-top: -22px' ?> ">
                 <div id="cert-qrcode" style="width: 160px; font-size: 22px; padding-left: -56px; font-weight: bold;"><img src="<?= $qrCode->writeDataUri() ?>"></div>
                 <div id="cert-number" style="padding-top: 0"> №<?= $testAssignment->id ?></div>
-                <div id="cert-date" style="padding-top: 5px;"><?= date('d.m.Y') ?> жыл</div>
+                <div id="cert-date" style="padding-top: 5px; padding-left: -10px"><?= date('d.m.Y') ?> жыл</div>
             </div>
         </div>
     </div>
