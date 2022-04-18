bbe7a80383abfd0b04606f3d7c1220340e1c377c
Jake <zhandos38@gmail.com>
2022-04-18 13:29:42 +0600

    fixes

--- frontend/controllers/OlympiadController.php
+++ frontend/controllers/OlympiadController.php
@@ -127,7 +127,7 @@ class OlympiadController extends Controller
             }
 
             $testAssignment = TestAssignment::findOne(['iin' => $model->iin, 'test_option_id' => $testOption->id, 'status' => TestAssignment::STATUS_FINISHED]);
-            if (!!$testAssignment) {
+            if ($testAssignment) {
                 Yii::$app->session->setFlash('error', Yii::t('app', 'Тест уже пройден'));
                 return $this->render('assignment', [
                     'model' => $model,
