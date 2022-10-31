67fecf0d34556dbee3d5c373834237ce756c2648
zhandos <zhandos38@users.noreply.github.com>
2022-10-31 10:47:43 +0600

    Update _cert-thank.php

--- frontend/views/marathon/_cert-thank.php
+++ frontend/views/marathon/_cert-thank.php
@@ -8,18 +8,27 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $t
     ->setMargin(5);
 ?>
 <div>
-    <div class="cert-page" style="background-image: url('./img/thanks.jpg'); background-size: cover; background-repeat: no-repeat; font-family: 'Times New Roman'; height: 1200px">
+    <div class="cert-page" style="background-image: url('./marathon-2022/marathon-certificate-2022.jpg'); background-size: cover; background-repeat: no-repeat; font-family: 'Times New Roman'; height: 1200px">
         <div style="padding-top: 220px; padding-left: 420px; text-align: center; width: 520px;">
-            <div style="font-size: 14px; font-weight: bold; height: 60px"><?= $testAssignment->school->name ?></div>
-            <div style="font-size: 14px; font-weight: bold; height: 60px"><?= $testAssignment->testOption->test ?></div>
             <div id="cert-name" style="height: 80px; font-size: 22px; font-weight: bold; text-transform: uppercase; color: #0096cb">
-                <?= $testAssignment->leader_name ?>
+                <?= $marathon->name ?> <?= $marathon->surname ?> <?= $marathon->patronymic ?>
             </div>
+            <div style="padding-top: 10px; font-size: "14px">
+                    <?php
+                    if ($marathon->school !== null) {
+                        if ($marathon->school->city_id === 1 || $marathon->school->city_id === 2 || $marathon->school->city_id === 3) {
+                            echo $marathon->school->city->name;
+                        } else {
+                            echo $marathon->school->city->region->name . ', ' . $marathon->school->city->name;
+                        }
+                    } ?>
+                </div>
+            <div style="font-size: 14px; font-weight: bold; height: 60px"><?= $marathon->school->name ?></div>
         </div>
-        <div id="footer" style="padding-top: 205px; padding-left: 25px; font-size: 16px; color: #454545; font-family: 'Times New Roman'">
-            <div id="cert-qrcode" style="padding-top: 5px; width: 160px; font-size: 22px; font-weight: bold;"><img src="<?= $qrCode->writeDataUri() ?>"></div>
-            <div id="cert-number" style="padding-top: 5px">Тіркеу №<?= $testAssignment->id ?></div>
-            <div id="cert-date" style="padding-top: 5px;"><?= date('d.m.Y') ?> жыл</div>
+        <div id="footer" style="padding-top: 205px; padding-left: 600px; font-size: 16px; color: #454545; font-family: 'Times New Roman'">
+            <div id="cert-number">Тіркеу №<?= $marathon->id ?></div>
+            <div id="cert-date"><?= date('d.m.Y') ?> жыл</div>
+            <div id="cert-qrcode" style="width: 160px; font-size: 22px; font-weight: bold;"><img src="<?= $qrCode->writeDataUri() ?>"></div>
         </div>
     </div>
 </div>
