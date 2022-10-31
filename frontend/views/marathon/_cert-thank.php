cf04d17809a460b2638c01afda78e9f82cb9e3c4
zhandos <zhandos38@users.noreply.github.com>
2022-10-31 11:02:45 +0600

    Update _cert-thank.php

--- frontend/views/marathon/_cert-thank.php
+++ frontend/views/marathon/_cert-thank.php
@@ -9,11 +9,12 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $m
 ?>
 <div>
     <div class="cert-page" style="background-image: url('/img/marathon-2022/marathon-certificate-2022.jpg'); background-size: cover; background-repeat: no-repeat; font-family: 'Times New Roman'; height: 1200px">
-        <div style="padding-top: 220px; padding-left: 420px; text-align: center; width: 520px;">
+        <div style="padding-top: 220px; padding-left: 400px; text-align: center; width: 520px; text-align: center;">
             <div id="cert-name" style="height: 80px; font-size: 22px; font-weight: bold; text-transform: uppercase; color: red">
                 <?= $marathon->name ?> <?= $marathon->surname ?> <?= $marathon->patronymic ?>
             </div>
             <div style="padding-top: 10px; font-size: "14px">
+                   <?= var_dump($marathon->school) ?>
                     <?php
                     if ($marathon->school !== null) {
                         if ($marathon->school->city_id === 1 || $marathon->school->city_id === 2 || $marathon->school->city_id === 3) {
