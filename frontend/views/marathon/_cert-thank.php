1f60a9be87c6f0161f3c4f371005d546b96b62fa
zhandos <zhandos38@users.noreply.github.com>
2022-10-31 11:05:34 +0600

    Update _cert-thank.php

--- frontend/views/marathon/_cert-thank.php
+++ frontend/views/marathon/_cert-thank.php
@@ -3,13 +3,13 @@ use Da\QrCode\QrCode;
 
 /* @var $marathon \common\models\Mararthon */
 
-$qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $marathon->id], 'https')))
+$qrCode = (new QrCode(\yii\helpers\Url::toRoute(['marathon/get-cert-thanl', 'id' => $marathon->id], 'https')))
     ->setSize(70)
     ->setMargin(5);
 ?>
 <div>
     <div class="cert-page" style="background-image: url('/img/marathon-2022/marathon-certificate-2022.jpg'); background-size: cover; background-repeat: no-repeat; font-family: 'Times New Roman'; height: 1200px">
-        <div style="padding-top: 220px; padding-left: 400px; text-align: center; width: 520px; text-align: center;">
+        <div style="padding-top: 220px; padding-left: 320px; text-align: center; width: 520px; text-align: center;">
             <div id="cert-name" style="height: 80px; font-size: 22px; font-weight: bold; text-transform: uppercase; color: red">
                 <?= $marathon->name ?> <?= $marathon->surname ?> <?= $marathon->patronymic ?>
             </div>
@@ -26,7 +26,7 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $m
                 </div>
             <div style="font-size: 14px; font-weight: bold; height: 60px"><?= $marathon->school->name ?></div>
         </div>
-        <div id="footer" style="padding-top: 205px; padding-left: 600px; font-size: 16px; color: #454545; font-family: 'Times New Roman'">
+        <div id="footer" style="padding-top: 505px; padding-left: 500px; font-size: 16px; color: #454545; font-family: 'Times New Roman'">
             <div id="cert-number">Тіркеу №<?= $marathon->id ?></div>
             <div id="cert-date"><?= date('d.m.Y') ?> жыл</div>
             <div id="cert-qrcode" style="width: 160px; font-size: 22px; font-weight: bold;"><img src="<?= $qrCode->writeDataUri() ?>"></div>
