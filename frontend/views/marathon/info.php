281d3a5d708609db11c784d9e60b3735e81f032b
zhandos <zhandos38@gmail.com>
2022-11-06 19:13:11 +0600

    fixes

---
+++ frontend/views/marathon/info.php
@@ -0,0 +1,161 @@
+<?php
+$this->params['heroTitle'] = 'Марафон 2021';
+$this->params['heroDescription'] = 'Ақпарат';
+?>
+<div class="row">
+    <div class="col-md-12">
+        <div class="accordion" id="accordionExample">
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingOne">
+                    <button class="accordion-button" type="button" data-bs-toggle="collapse" data-bs-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+                        1. «КАНИКУЛДА КІТАП ОҚИМЫЗ» МАРАФОН-БАЙҚАУЫ
+                    </button>
+                </h2>
+                <div id="collapseOne" class="accordion-collapse collapse show" aria-labelledby="headingOne" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        Жауап:  Өз таңдауыңыздағы олимпиадаға барып, логотип астындағы Ереже батырмасын басыңыз, Ережемен толық танысып шығыңыз. Біздің талаптармен келіскен жағдайда қатысуыңызға болады.
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwo">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwo" aria-expanded="false" aria-controls="collapseTwo">
+                        2. «КАНИКУЛДА КІТАП ОҚИМЫЗ» марафон-байқауының мақсаты.
+                    </button>
+                </h2>
+                <div id="collapseTwo" class="accordion-collapse collapse" aria-labelledby="headingTwo" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingThree">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseThree" aria-expanded="false" aria-controls="collapseThree">
+                        3. «КАНИКУЛДА КІТАП ОҚИМЫЗ» марафон-байқауын өткізу масштабы
+                        – Республика көлемінде. Бұқаралығы - 2-11 сынып оқушылары.
+                    </button>
+                </h2>
+                <div id="collapseThree" class="accordion-collapse collapse" aria-labelledby="headingThree" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingFour">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseFour" aria-expanded="false" aria-controls="collapseFour">
+                        4. Өткізу ережелері мен шарттары
+                    </button>
+                </h2>
+                <div id="collapseFour" class="accordion-collapse collapse" aria-labelledby="headingFour" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSix">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSix" aria-expanded="false" aria-controls="collapseSix">
+                        5. Марафон-байқаудың бағалау критерийлері
+                    </button>
+                </h2>
+                <div id="collapseSix" class="accordion-collapse collapse" aria-labelledby="headingSix" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingSeven">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseSeven" aria-expanded="false" aria-controls="collapseSeven">
+                        6. Марафон-байқаудың көтермелеу жүйесі, өткен жылдардың жеңімпаздары мен жүлдегерлері туралы ақпараттар.
+                    </button>
+                </h2>
+                <div id="collapseSeven" class="accordion-collapse collapse" aria-labelledby="headingSeven" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingEight">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEight" aria-expanded="false" aria-controls="collapseEight">
+                        7. Апелляцияға сілтеме.
+                    </button>
+                </h2>
+                <div id="collapseEight" class="accordion-collapse collapse" aria-labelledby="headingEight" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <a href="https://wa.me/77754037284">https://wa.me/77754037284</a>
+                        <br>
+                        <a href="https://wa.me/77753129906">https://wa.me/77753129906</a>
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingNine">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseNine" aria-expanded="false" aria-controls="collapseNine">
+                        8. Марафон-байқауға қатысушылары педагогтердің әдістемелік, ақпараттық қолдауы. 2021 жыл Меморандум
+                    </button>
+                </h2>
+                <div id="collapseNine" class="accordion-collapse collapse" aria-labelledby="headingNine" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTen">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTen" aria-expanded="false" aria-controls="collapseTen">
+                        9. Оқушылары каникулда 100 пайыз кітап оқыған мектептер тізімі.
+                    </button>
+                </h2>
+                <div id="collapseTen" class="accordion-collapse collapse" aria-labelledby="headingTen" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingEleven">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseEleven" aria-expanded="false" aria-controls="collapseEleven">
+                        10. Марафон жеңімпаздарын марапаттау.
+                    </button>
+                </h2>
+                <div id="collapseEleven" class="accordion-collapse collapse" aria-labelledby="headingEleven" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+
+                    </div>
+                </div>
+            </div>
+            <div class="accordion-item">
+                <h2 class="accordion-header" id="headingTwelve">
+                    <button class="accordion-button collapsed" type="button" data-bs-toggle="collapse" data-bs-target="#collapseTwelve" aria-expanded="false" aria-controls="collapseTwelve">
+                        11. «КАНИКУЛДА КІТАП ОҚИМЫЗ» марафон-байқауы бойынша әлеуеметтік желідегі пікірлер.
+                    </button>
+                </h2>
+                <div id="collapseTwelve" class="accordion-collapse collapse" aria-labelledby="headingTwelve" data-bs-parent="#accordionExample">
+                    <div class="accordion-body">
+                        <div>
+                            2021 жыл:
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;color:black;'>-</span> <a href="https://www.facebook.com/100015419883057/posts/pfbid02be56EXYayuyVhavxFNp2V7arWhdQ1KMt54Qm1ScQgMua8vQWRHT1XkN4agLn9Rkzl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100015419883057/posts/pfbid02be56EXYayuyVhavxFNp2V7arWhdQ1KMt54Qm1ScQgMua8vQWRHT1XkN4agLn9Rkzl/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;color:black;'>-</span> <a href="https://www.facebook.com/100029004851547/posts/pfbid02dULEGVGCSoXNj7Nu67xMMsFAKHt7BNzJPcJpGnNeyY7gvCa975SoP4d27jPtHoZZl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100029004851547/posts/pfbid02dULEGVGCSoXNj7Nu67xMMsFAKHt7BNzJPcJpGnNeyY7gvCa975SoP4d27jPtHoZZl/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;color:black;'>-</span> <a href="https://www.facebook.com/100071551248592/posts/pfbid02MeK4Qrxga7BywNMFQAxUfbTX5UsPX3G2nHCszayiZY9RyVEMuRophBgyF9mbWsh7l/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100071551248592/posts/pfbid02MeK4Qrxga7BywNMFQAxUfbTX5UsPX3G2nHCszayiZY9RyVEMuRophBgyF9mbWsh7l/?d=n</span></a></p>
+                        </div>
+                        <div>
+                            2022 жыл:
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>&nbsp;</span><a href="https://www.facebook.com/100052910271312/posts/pfbid02BizZoD1wvG2yhQEVBqDYKSPNRxgupLtvbvFWPSnKBnQ9kmMzk3mfgu4E6fgAwVNNl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100052910271312/posts/pfbid02BizZoD1wvG2yhQEVBqDYKSPNRxgupLtvbvFWPSnKBnQ9kmMzk3mfgu4E6fgAwVNNl/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span> <a href="https://www.facebook.com/100063642390673/posts/pfbid0tKg1v9kqveAGoGi4iWKphtKVbN79HVpq7tLaC2VneK3UvSecT1wguZsy9YCawLF5l/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100063642390673/posts/pfbid0tKg1v9kqveAGoGi4iWKphtKVbN79HVpq7tLaC2VneK3UvSecT1wguZsy9YCawLF5l/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span> <a href="https://www.facebook.com/100074672530470/posts/pfbid0g95EntveAL45VHhAqpUUtgXo82ETtxaLVv5bdUSFTGDBctP1G6VQznBGWzQVxZbl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100074672530470/posts/pfbid0g95EntveAL45VHhAqpUUtgXo82ETtxaLVv5bdUSFTGDBctP1G6VQznBGWzQVxZbl/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span> <a href="https://www.facebook.com/109271831538728/posts/pfbid0327apVZx66BJbn9TeAh4DRHyQiZSVHuoavnUJuLybBFY7BUqCeZqRTQqnYDEZAo8kl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/109271831538728/posts/pfbid0327apVZx66BJbn9TeAh4DRHyQiZSVHuoavnUJuLybBFY7BUqCeZqRTQqnYDEZAo8kl/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span> <a href="https://www.facebook.com/100048235103559/posts/pfbid0Zr6yqaJCmQV3C1kctwxE14q9nVar8r8xSh9wbPg9F2qp2jiDdczxQXH8NR5B1AN2l/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100048235103559/posts/pfbid0Zr6yqaJCmQV3C1kctwxE14q9nVar8r8xSh9wbPg9F2qp2jiDdczxQXH8NR5B1AN2l/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span> <a href="https://www.facebook.com/109271831538728/posts/pfbid02RSb7XeG76G6iDbKKNggx6EcsV92iTvLsXpCodEtztEiEgbthohhD8XfDwgBqHMiEl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/109271831538728/posts/pfbid02RSb7XeG76G6iDbKKNggx6EcsV92iTvLsXpCodEtztEiEgbthohhD8XfDwgBqHMiEl/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span> <a href="https://www.facebook.com/109271831538728/posts/pfbid02PmB24s38zCE1ro4sYYyTXzXQ5E1tG26j7SjkpMG69LsFh3ss5q8ATb4QxB4nqA9Nl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/109271831538728/posts/pfbid02PmB24s38zCE1ro4sYYyTXzXQ5E1tG26j7SjkpMG69LsFh3ss5q8ATb4QxB4nqA9Nl/?d=n</span></a></p>
+                            <p style='margin-top:0cm;margin-right:0cm;margin-bottom:0cm;margin-left:0cm;line-height:115%;font-size:15px;font-family:"Calibri",sans-serif;'><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>-</span> <a href="https://www.facebook.com/100040819761746/posts/pfbid0mM3qC2pkUm6iP9GmYu6W5wWRkgcYuZH2Q2NzFg9nnjUZc44azC1i5YfyMJWmGXGgl/?d=n"><span style='font-size:16px;line-height:115%;font-family:"Times New Roman",serif;'>https://www.facebook.com/100040819761746/posts/pfbid0mM3qC2pkUm6iP9GmYu6W5wWRkgcYuZH2Q2NzFg9nnjUZc44azC1i5YfyMJWmGXGgl/?d=n</span></a></p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
