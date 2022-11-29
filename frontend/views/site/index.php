0ce45b59ec84dcbb456f7e92d93935b3312b2c6b
jake <zh.timabay@mycar.kz>
2022-11-29 12:15:57 +0600

    fixes

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -91,7 +91,7 @@ $this->registerMetaTag([
                              data-frames='[{"delay":1200,"speed":1000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[-100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
                              data-type="text"
                              data-x="['left','center','center','center']" data-hoffset="['0','0','0','0']"
-                             data-y="['middle','middle','middle','middle']" data-voffset="['45','35','-5','15']"
+                             data-y="['middle','middle','middle','middle']" data-voffset="['65','45','-5','15']"
                              data-width="auto"
                              data-height="auto"
                              data-fontsize="['32','16','19','14']"
