d7ed359c726c12ed17581f98241394c990c5f512
jake <zh.timabay@mycar.kz>
2022-11-29 11:54:11 +0600

    fixes

--- frontend/views/site/index.php
+++ frontend/views/site/index.php
@@ -22,68 +22,9 @@ $this->registerMetaTag([
                     <!-- start slide 02 -->
                     <li data-index="rs-73" data-transition="zoomout" data-slotamount="default" data-hideafterloop="0" data-hideslideonmobile="off"  data-easein="default" data-easeout="default" data-masterspeed="1500"  data-thumb="http://works.themepunch.com/revolution_5_3/wp-content/"  data-rotate="0"  data-saveperformance="off"  data-title="01" data-param1="01" data-description="">
                         <!-- main image -->
-                        <img src="/img/teacher-banner-bg3.jpg" alt="" data-bgcolor="#262b32" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="off" class="rev-slidebg" data-no-retina>
+                        <img src="/img/altyn-qyran-2022/banner.jpg" alt="" data-bgcolor="#262b32" data-bgposition="center center" data-bgfit="cover" data-bgrepeat="no-repeat" data-bgparallax="off" class="rev-slidebg" data-no-retina>
 
                         <div class="overlay-bg bg-extra-dark-gray" style="opacity: 0.3;"></div>
-
-                        <!-- help text layer -->
-                        <div class="tp-caption tp-resizeme alt-font text-black font-weight-600 text-white"
-                             id="slide-411-layer-01"
-                             data-frames='[{"delay":200,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[-100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                             data-type="text"
-                             data-whitespace="nowrap"
-                             data-x="['left','center','center','center']" data-hoffset="['0','0','0','0']"
-                             data-y="['middle','middle','middle','middle']" data-voffset="['-160','-160','-160','-140']"
-                             data-width="auto"
-                             data-height="auto"
-                             data-fontsize="['38','26','26','26']"
-                             data-lineheight="['70','59','70','39']"
-                             data-letterspacing="['0','-1','-1','-1']"
-                             data-responsive="on"
-                             data-responsive_offset="on"
-                             data-paddingtop="['0','0','0','0']"
-                             data-paddingbottom="['15','8','8','8']"
-                             data-paddingright="['0','0','0','0']"
-                             data-paddingleft="['0','0','0','0']"
-                             style="text-shadow: 0 0 20px rgba(0,0,0,0.3);">ҚЫМБАТТЫ ОҚУШЫ</div>
-
-                        <!-- main text layer -->
-                        <div class="tp-caption tp-resizeme alt-font text-white"
-                             id="slide-411-layer-01"
-                             data-frames='[{"delay":200,"speed":2000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[-100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                             data-type="text"
-                             data-whitespace="nowrap"
-                             data-x="['left','center','center','center']" data-hoffset="['0','0','0','0']"
-                             data-y="['middle','middle','middle','middle']" data-voffset="['-50','-50','-115','-65']"
-                             data-width="auto"
-                             data-height="auto"
-                             data-fontsize="['32','32','32','24']"
-                             data-lineheight="['59','59','70','39']"
-                             data-letterspacing="['0','-1','-1','-1']"
-                             data-responsive="on"
-                             data-responsive_offset="on"
-                             data-paddingtop="['0','0','80','0']"
-                             data-paddingbottom="['15','8','8','8']"
-                             data-paddingright="['0','0','0','0']"
-                             data-paddingleft="['0','0','0','0']"
-                             style="text-shadow: 0 0 20px rgba(0,0,0,0.3);">"БІЛІМ ШЫҢЫ" олимпиадасы қатысып, <br> Бізбен бірге БІЛІМ ШЫҢЫ бағындыр!!!</div>
-
-                        <!-- small text layer -->
-                        <div class="tp-caption tp-resizeme alt-font text-white"
-                             id="slide-411-layer-02"
-                             data-frames='[{"delay":1200,"speed":1000,"frame":"0","from":"y:[100%];z:0;rX:0deg;rY:0;rZ:0;sX:1;sY:1;skX:0;skY:0;opacity:0;","mask":"x:0px;y:[-100%];s:inherit;e:inherit;","to":"o:1;","ease":"Power2.easeInOut"},{"delay":"wait","speed":300,"frame":"999","to":"auto:auto;","ease":"Power3.easeInOut"}]'
-                             data-type="text"
-                             data-x="['left','center','center','center']" data-hoffset="['0','0','0','0']"
-                             data-y="['middle','middle','middle','middle']" data-voffset="['85','100','-5','15']"
-                             data-width="auto"
-                             data-height="auto"
-                             data-fontsize="['32','16','19','14']"
-                             data-lineheight="['28','14','23','20']"
-                             data-letterspacing="['0.5','0.5','0.5','0.5']"
-                             data-responsive="off"
-                             data-responsive_offset="on">
-                            Өтетін уақыты 5-15 желтоқсан, 2022 жылы
-                        </div>
 
                         <!-- btn layer -->
                         <a class="tp-caption tp-resizeme rs-btn btn btn-rounded d-flex align-items-center justify-content-center"
