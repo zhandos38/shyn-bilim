6bf79998a72d98abb9dd0d2b7a6dfa5f0c6a02ce
zhandos <zhandos38@gmail.com>
2023-01-26 23:47:32 +0600

    article cert fixes

--- frontend/views/article/_cert.php
+++ frontend/views/article/_cert.php
@@ -9,13 +9,7 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['article/cert', 'id' => $model-
 ?>
 <div class="cert-page" style="background-image: url('/img/article/cert.jpg'); background-size: cover; background-repeat: no-repeat; width: 1400px; height: 800px">
     <div id="cert-name" class="bordered" style="padding-top: 300px; padding-left: 40px; width: 600px; text-align: center">
-        <span style="font-size: 28px; font-weight: 500; color: #000">
-            <?= $model->surname . ' ' . $model->name . ' ' . $model->patronymic ?>
-        </span>
-        <div style="text-transform: uppercase; font-weight: bold">
-            <?= $model->subject->name ?> пәні мұғалімі
-        </div>
-        <div id="cert-city" style="padding-top: 10px; font-size: 14px; color: #000000; height: 10px; text-align: center;">
+        <div id="cert-city" style="padding-top: 0; font-size: 14px; color: #000000; height: 10px; text-align: center;">
             <?php
             if ($model->school !== null) {
                 if ($model->school->city_id === 1 || $model->school->city_id === 2 || $model->school->city_id === 3) {
@@ -26,8 +20,14 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['article/cert', 'id' => $model-
             }
             ?>
         </div>
-        <div id="cert-school" style="font-size: 14px; color: #000000; height: 40px; text-align: center; line-height: 110%;">
+        <div id="cert-school" style="font-size: 14px; color: #000000; height: 50px; text-align: center; line-height: 110%;">
             <?= $model->school->name ?>
+        </div>
+        <span style="font-size: 28px; font-weight: 500; color: #000">
+            <?= $model->surname . ' ' . $model->name . ' ' . $model->patronymic ?>
+        </span>
+        <div style="text-transform: uppercase; font-weight: bold">
+            <?= $model->subject->name ?> пәні мұғалімі
         </div>
     </div>
     <div id="cert-topic" style="padding-top: 20px; padding-left: 40px; font-size: 18px; color: #000000; line-height: 110%; width: 600px; height: 60px; text-align: center">
