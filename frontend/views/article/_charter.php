f223e09fb13a2c6c81b453456c953b38bb14dd6f
zhandos <zhandos38@users.noreply.github.com>
2023-01-27 16:12:53 +0600

    Update _charter.php

--- frontend/views/article/_charter.php
+++ frontend/views/article/_charter.php
@@ -8,7 +8,7 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['article/charter', 'id' => $mod
     ->setMargin(5);
 ?>
 <div class="cert-page" style="background-image: url('/img/article/charter2.jpg'); background-size: cover; background-repeat: no-repeat; width: 1400px; height: 800px; font-family: 'Times New Roman';">
-    <div id="cert-name" class="bordered" style="padding-top: 580px; padding-left: 260px; width: 480px; text-align: center">
+    <div id="cert-name" class="bordered" style="padding-top: 600px; padding-left: 300px; width: 460px; text-align: center">
         <div id="cert-city" style="padding-top: 0; font-size: 14px; color: #000000; height: 10px; text-align: center;">
             <?php
             if ($model->school !== null) {
