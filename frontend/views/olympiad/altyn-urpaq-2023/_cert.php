7f8464c122d6ac70381f10da063a994afbc0d8e3
zhandos <zhandos38@users.noreply.github.com>
2023-01-31 14:25:41 +0600

    Update _cert.php

--- frontend/views/olympiad/altyn-urpaq-2023/_cert.php
+++ frontend/views/olympiad/altyn-urpaq-2023/_cert.php
@@ -42,9 +42,9 @@ if ($testAssignment->grade >= 1 && $testAssignment->grade <= 4) {
                 <?= $testAssignment->grade ?> сынып оқушысы
             </div>
         </div>
-        <div class="border" style="display: flex; padding-left: 20px; padding-top: 260px; color: #fff9f6">
+        <div class="border" style="display: flex; padding-left: 20px; padding-top: 280px; color: #fff9f6">
             <div id="cert-qrcode"><?= '<img src="' . $qrCode->writeDataUri() . '">' ?></div>
-            <div style="color: #0a0a0a; padding-top: -45px; font-size: 14px; padding-left: 100px">
+            <div style="color: #0a0a0a; padding-top: -45px; font-size: 12px; padding-left: 100px">
                 <div id="cert-number" style="color: #0a0a0a">Тіркеу №<?= $testAssignment->id ?></div>
                 <div id="cert-date" style="color: #0a0a0a;">Күні <?= date('d.m.Y') ?></div>
             </div>
