1c7dc7deaec0fca92b8c6ece4202bb8e940e1c46
zhandos <zhandos38@users.noreply.github.com>
2023-02-01 11:23:27 +0600

    Update check-cert-thank-parent.php

--- frontend/views/olympiad/check-cert-thank-parent.php
+++ frontend/views/olympiad/check-cert-thank-parent.php
@@ -27,10 +27,6 @@ $this->params['breadcrumbs'][] = ['label' => $this->title, 'url' => '#'];
 
         <?= $form->field($model, 'iin') ?>
 
-        <?= $form->field($model, 'subject_id')->dropDownList(ArrayHelper::map(\common\models\Subject::findAll(['type' => \common\models\Subject::TYPE_STUDENT]), 'id', 'name'), [
-            'id' => 'subject_id-select',
-            'prompt' => Yii::t('app', 'Выберите предмет')
-        ]) ?>
         <?= Html::submitButton(Yii::t('app', 'Отправить'), ['class' => 'btn btn-primary']) ?>
     </div>
 </div>
