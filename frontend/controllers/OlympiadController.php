acae48a7a5e9d4baa0d621a3d834c6e24994186a
zhandos <zhandos38@gmail.com>
2023-02-04 12:37:05 +0600

    fixes

--- frontend/controllers/OlympiadController.php
+++ frontend/controllers/OlympiadController.php
@@ -344,7 +344,7 @@ class OlympiadController extends Controller
 
         if ($testAssignment->status !== TestAssignment::STATUS_FINISHED) {
             Yii::$app->session->setFlash('success', 'Тест аяқталмаған немесе төленбеген');
-            return $this->redirect(['olympiad/index']);
+            return $this->redirect(['/']);
         }
 
         if ($testAssignment->point >= 10) {
@@ -389,6 +389,11 @@ class OlympiadController extends Controller
                 'cssFile' => 'css/custom.css'
             ]);
         } else {
+            if ($testAssignment->grade === 20) {
+                Yii::$app->session->setFlash('success', 'Скачайте сертификат со второго тура');
+                return $this->redirect(['/']);
+            }
+
             $content = $this->renderPartial($testAssignment->olympiad->getFolderPath('_cert'), [
                 'testAssignment' => $testAssignment,
             ]);
