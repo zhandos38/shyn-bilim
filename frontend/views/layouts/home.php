949c0ebc7e66ee2d00789728787a1f001f4b45f0
zhandos <zhandos38@gmail.com>
2023-10-14 09:23:52 +0600

    fixes

--- frontend/views/layouts/home.php
+++ frontend/views/layouts/home.php
@@ -220,7 +220,14 @@ AppAsset::register($this);
 <?php include_once "_mobile-menu.php" ?>
 
 <?= Alert::widget() ?>
+
 <?= $content ?>
+
+<div class="rbt-progress-parent">
+    <svg class="rbt-back-circle svg-inner" width="100%" height="100%" viewBox="-1 -1 102 102">
+        <path d="M50,1 a49,49 0 0,1 0,98 a49,49 0 0,1 0,-98" />
+    </svg>
+</div>
 
 <?= $this->render('_footer') ?>
 
