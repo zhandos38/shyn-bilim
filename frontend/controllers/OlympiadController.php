fafa15232f64dc52d8aa7abb0406678bac377bcd
zhandos <zhandos38@gmail.com>
2024-02-28 11:39:03 +0600

    fixes

--- frontend/controllers/OlympiadController.php
+++ frontend/controllers/OlympiadController.php
@@ -140,9 +140,35 @@ class OlympiadController extends Controller
             }
 
             $model->created_at = time();
-            $model->status = TestAssignment::STATUS_ACTIVE;
+
+            // Check whitelist
+            $whiteList = WhiteList::findOne(['iin' => $model->iin]);
+            if ($whiteList !== null) {
+                $model->status = TestAssignment::STATUS_ACTIVE;
+            }
+
             if (!$model->save()) {
                 throw new Exception('Assignment is not saved');
+            }
+
+            if ($whiteList === null) {
+                $salt = $this->getSalt(8);
+                $request = [
+                    'pg_merchant_id' => Yii::$app->params['payboxId'],
+                    'pg_amount' => $olympiad->price,
+                    'pg_salt' => $salt,
+                    'pg_order_id' => $model->id,
+                    'pg_description' => 'Оплата за участие в олимпиаде',
+                    'pg_success_url' => Url::base('https') . '/olympiad/success',
+                    'pg_result_url' => Yii::$app->params['apiDomain'] . '/olympiad/result',
+                    'pg_result_url_method' => 'POST',
+                ];
+
+                $request = $this->getSignByData($request, 'payment.php', $salt);
+
+                $query = http_build_query($request);
+
+                return $this->redirect('https://api.paybox.money/payment.php?' . $query);
             }
 
             return $this->redirect(['test', 'assignment' => $model->id]);
