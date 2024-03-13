cd7a315244ed7727e9bd6ce5c98b3f11cf8b177a
zhandos <zhandos38@gmail.com>
2024-03-13 16:56:41 +0500

    fixes

--- frontend/views/article/bilimshini/_charter.php
+++ frontend/views/article/bilimshini/_charter.php
@@ -7,7 +7,7 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['article/charter', 'id' => $mod
     ->setSize(80)
     ->setMargin(5);
 ?>
-<div class="cert-page" style="background-image: url('/img/article/charter.jpg'); background-size: cover; background-repeat: no-repeat; width: 1400px; height: 1200px; font-family: 'Times New Roman';">
+<div class="cert-page" style="background-image: url('/img/article/bilimshini/charter.jpg'); background-size: cover; background-repeat: no-repeat; width: 1400px; height: 1200px; font-family: 'Times New Roman';">
     <div id="cert-name" style="height: 200px; padding-top: 540px; padding-left: 170px; width: 460px; text-align: center">
         <div id="cert-city" style="padding-top: 0; font-size: 18px; color: #000000; text-align: center;">
             <?php
