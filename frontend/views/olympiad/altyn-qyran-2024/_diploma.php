6bd2d373c32f2ee5ae8545419347cb1a6b505c52
zhandos <zhandos38@gmail.com>
2024-10-13 22:42:35 +0500

    fixes

--- frontend/views/olympiad/altyn-qyran-2024/_diploma.php
+++ frontend/views/olympiad/altyn-qyran-2024/_diploma.php
@@ -15,14 +15,14 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $t
         <div style="font-size: 28px; padding-left: 20px; padding-top: 360px; text-transform: uppercase; color: red; text-align: center">
             <b><?= $place ?></b>
         </div>
-        <div style="padding-left: 120px; padding-top: 130px; text-align: center; width: 600px; height: 150px; font-size: 20px">
+        <div style="padding-left: 100px; padding-top: 130px; text-align: center; width: 600px; height: 150px; font-size: 20px">
             <div style="padding-top: 50px; font-weight: lighter; text-transform: uppercase;">
                 <b><?= $testAssignment->surname . ' ' . $testAssignment->name . ' ' . $testAssignment->patronymic ?></b>
             </div>
             <div style="font-size: 16px; padding-top: 10px;">
                 <?= $testAssignment->grade ?> сынып оқушысы
             </div>
-            <div style="padding-left: 100px; font-family: 'Arial'; height: 100px; width: 400px; font-size: 16px">
+            <div style="padding-left: 50px; font-family: 'Arial'; height: 100px; width: 500px; font-size: 16px">
                 <div style="font-weight: 500;">
                     <?php
                     if ($testAssignment->school !== null) {
