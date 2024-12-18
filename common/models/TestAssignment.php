39ec7a629e4a6030d4bc920d4f04ab2025d7c466
zhandos <zhandos38@gmail.com>
2024-12-18 22:41:50 +0500

    fixes

--- common/models/TestAssignment.php
+++ common/models/TestAssignment.php
@@ -67,7 +67,7 @@ class TestAssignment extends \yii\db\ActiveRecord
             ['lang', 'string', 'max' => 2],
             [['city_id', 'region_id', 'status'], 'integer'],
 
-            [['iin', 'name', 'surname', 'phone', 'lang', 'school_id', 'teacher_name', 'leader_phone', 'grade'], 'required'],
+            [['iin', 'name', 'surname', 'phone', 'lang', 'school_id'], 'required'],
         ];
     }
 
