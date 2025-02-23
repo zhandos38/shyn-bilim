f59852e75145f450093246aa350cbca30b78b887
zhandos <zhandos38@gmail.com>
2025-02-23 16:08:18 +0500

    fixes

--- frontend/views/olympiad/en-bilimdi-pedagog-2025/_diploma.php
+++ frontend/views/olympiad/en-bilimdi-pedagog-2025/_diploma.php
@@ -14,9 +14,9 @@ $olympiadId = $testAssignment->olympiad_id;
 $cityId = $testAssignment->school->city_id;
 $regionId = $testAssignment->school->city->region_id;
 $template = 'diploma.jpg';
-if ($olympiadId === 25 && $cityId === 3) {
+if ($olympiadId === 25 && $testAssignment->point === 30 && $cityId === 3) {
     $template = "diploma-shymkent.jpg";
-} else if ($olympiadId === 25 && $regionId === 14) {
+} else if ($olympiadId === 25 && $testAssignment->point === 30 && $regionId === 14) {
     $template = "diploma-turkestan.jpg";
 }
 ?>
