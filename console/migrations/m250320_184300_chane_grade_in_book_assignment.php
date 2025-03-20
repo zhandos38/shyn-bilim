300d8f74e68752a2771df0b6eda8009eab92a40a
root <root@server.bilimshini.kz>
2025-03-20 18:43:20 +0000

    fixes

---
+++ console/migrations/m250320_184300_chane_grade_in_book_assignment.php
@@ -0,0 +1,42 @@
+<?php
+
+use yii\db\Migration;
+
+/**
+ * Class m250320_184300_chane_grade_in_book_assignment
+ */
+class m250320_184300_chane_grade_in_book_assignment extends Migration
+{
+    /**
+     * {@inheritdoc}
+     */
+    public function safeUp()
+    {
+
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function safeDown()
+    {
+        echo "m250320_184300_chane_grade_in_book_assignment cannot be reverted.\n";
+
+        return false;
+    }
+
+    /*
+    // Use up()/down() to run migration code without a transaction.
+    public function up()
+    {
+
+    }
+
+    public function down()
+    {
+        echo "m250320_184300_chane_grade_in_book_assignment cannot be reverted.\n";
+
+        return false;
+    }
+    */
+}
