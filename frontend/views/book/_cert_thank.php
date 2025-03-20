00b44ef8398e727210757b7aee0022d1faa16ab7
Gauhar <zhandos38@gmail.com>
2025-03-21 01:36:13 +0500

    fixes

--- frontend/views/book/_cert_thank.php
+++ frontend/views/book/_cert_thank.php
@@ -9,11 +9,11 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['article/cert', 'id' => $model-
 ?>
 <div>
     <div class="cert-page" style="background-image: url('./img/kanikulda-kitap-oku-2025/oku/cert-thank.jpg'); background-size: cover; background-repeat: no-repeat; font-family: 'Arial'; width: 1200px; height: 800px">
-        <div style="padding-left: 260px; padding-top: 240px">
-            <div id="cert-name" style="padding-left: 0; height: 80px; font-size: 22px; text-transform: uppercase; text-align: left;">
+        <div style="padding-left: -260px; padding-top: 240px">
+            <div id="cert-name" style="padding-left: 0; height: 80px; font-size: 22px; text-transform: uppercase; text-align: center;">
                 <b><?= $model->leader_name ?></b>
             </div>
-            <div id="footer" style="text-align: left; padding-left: 40px; padding-top: 280px; font-size: 14px; font-family: 'Times New Roman'">
+            <div id="footer" style="text-align: left; padding-left: 240px; padding-top: 280px; font-size: 14px; font-family: 'Times New Roman'">
                 <div id="cert-qrcode"><img src="<?= $qrCode->writeDataUri() ?>" alt="qr"></div>
                 <div style="padding-left: 0">
                     <div id="cert-number" style="padding-top: 10px">Тіркеу №<?= $model->id ?></div>
