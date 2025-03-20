912901cba23fae2e422088e839475a4ec2b847be
zhandos <zhandos38@users.noreply.github.com>
2025-03-20 23:24:49 +0500

    fixes

---
+++ common/models/BookAssignment.php
@@ -0,0 +1,90 @@
+<?php
+
+namespace common\models;
+
+use Yii;
+use yii\helpers\ArrayHelper;
+
+/**
+ * This is the model class for table "book_assignment".
+ *
+ * @property int $id
+ * @property string $name
+ * @property string $surname
+ * @property string|null $patronymic
+ * @property string $leader_name
+ * @property string $leader_phone
+ * @property string|null $parent_name
+ * @property string|null $parent_phone
+ * @property string $iin
+ * @property int|null $school_id
+ * @property int $grade
+ * @property int|null $created_at
+ *
+ * @property School $school
+ */
+class TestAssignment extends \yii\db\ActiveRecord
+{
+    public $region_id;
+    public $city_id;
+
+    /**
+     * {@inheritdoc}
+     */
+    public static function tableName()
+    {
+        return 'book_assignment';
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function rules()
+    {
+        return [
+            [['school_id', 'grade','created_at'], 'integer'],
+            [['name', 'surname', 'patronymic', 'teacher_name', 'teacher_phone', 'parent_name', 'parent_phone', 'phone'], 'string', 'max' => 255],
+            [['iin'], 'string', 'max' => 20],
+
+            [['city_id', 'region_id'], 'integer'],
+
+            [['iin', 'name', 'surname', 'phone', 'school_id'], 'required'],
+        ];
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    public function attributeLabels()
+    {
+        return [
+            'id' => 'ID',
+            'name' => Yii::t('app', 'Имя'),
+            'surname' => Yii::t('app', 'Фамилия'),
+            'patronymic' => Yii::t('app', 'Отчество'),
+            'iin' => Yii::t('app', 'ИИН участника'),
+            'school_id' => Yii::t('app', 'Школа/Колледж'),
+            'grade' => Yii::t('app', 'Класс'),
+            'leader_name' => Yii::t('app', 'Ф.И.О преподавателя'),
+            'leader_phone' => Yii::t('app', 'Жетекші мұғалімнің телефон номері'),
+            'parent_name' => Yii::t('app', 'Оқушы ата-ананың аты-жөні (тек бір адам)'),
+            'parent_phone' => Yii::t('app', 'Оқушы ата-ананың телефон нөмері (тек бір адам)'),
+            'city_id' => Yii::t('app', 'Город'),
+            'region_id' => Yii::t('app', 'Регион'),
+            'created_at' => Yii::t('app', 'Время создание'),
+        ];
+    }
+
+    /**
+     * @return \yii\db\ActiveQuery
+     */
+    public function getSchool()
+    {
+        return $this->hasOne(School::className(), ['id' => 'school_id']);
+    }
+
+    public function getGrade()
+    {
+        return $this->grade . ' сынып оқушысы';
+    }
+}
--- console/migrations/m250320_175817_create_book_assignment_table.php
+++ console/migrations/m250320_175817_create_book_assignment_table.php
@@ -14,7 +14,20 @@ class m250320_175817_create_book_assignment_table extends Migration
     {
         $this->createTable('{{%book_assignment}}', [
             'id' => $this->primaryKey(),
+            'name' => $this->string(),
+            'surname' => $this->string(),
+            'patronymic' => $this->string()->null(),
+            'iin' => $this->string(),
+            'school_id' => $this->integer(),
+            'grade' => $this->integer(),
+            'leader_name' => $this->string(),
+            'leader_phone' => $this->string(),
+            'parent_name' => $this->string()->null(),
+            'parent_phone' => $this->string()->null(),
+            'created_at' => $this->integer()
         ]);
+
+        $this->addForeignKey('fk-book_assignment-school_id', '{{%book_assignment}}', 'school_id', 'school', 'id', 'SET NULL');
     }
 
     /**
