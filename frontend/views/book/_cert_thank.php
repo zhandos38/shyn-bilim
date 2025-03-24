1efbedc44cc1f442e727d246134664932b43f2e2
zhandos <zhandos38@gmail.com>
2025-03-24 12:24:05 +0500

    fixes

--- frontend/views/book/_cert_thank.php
+++ frontend/views/book/_cert_thank.php
@@ -11,7 +11,7 @@ $qrCode = (new QrCode(\yii\helpers\Url::toRoute(['article/cert', 'id' => $model-
     <div class="cert-page" style="background-image: url('./img/kanikulda-kitap-oku-2025/oku/cert-thank.jpg'); background-size: cover; background-repeat: no-repeat; font-family: 'Arial'; width: 1200px; height: 800px">
         <div style="padding-left: -260px; padding-top: 240px">
             <div id="cert-name" style="padding-left: 0; height: 80px; font-size: 22px; text-transform: uppercase; text-align: center;">
-                <b><?= $model->leader_name ?></b>
+                <b><?= $model->parent_name ?></b>
             </div>
             <div id="footer" style="text-align: left; padding-left: 320px; padding-top: 280px; font-size: 14px; font-family: 'Times New Roman'">
                 <div id="cert-qrcode"><img src="<?= $qrCode->writeDataUri() ?>" alt="qr"></div>
