91d4447f4d6052fc5c95b2de286668da93e1266a
zhandos <zhandos38@users.noreply.github.com>
2025-09-30 10:00:50 +0500

    Update _form.php

--- backend/views/white-list/_form.php
+++ backend/views/white-list/_form.php
@@ -12,7 +12,7 @@ use yii\widgets\ActiveForm;
 /* @var $model common\models\WhiteList */
 /* @var $form yii\widgets\ActiveForm */
 
-$model->olympiad_id = 27;
+$model->olympiad_id = 28;
 ?>
 
 <div class="white-list-form">
