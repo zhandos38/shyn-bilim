a145ec839313755decce1bde5254200f57baaeea
zhandos <zhandos38@gmail.com>
2025-10-06 15:56:40 +0500

    fixes

---
+++ frontend/views/olympiad/altyn-qyran-2025/_cert.php
@@ -0,0 +1,52 @@
+<?php
+
+use common\models\Subject;
+use Da\QrCode\QrCode;
+
+/* @var $testAssignment \common\models\TestAssignment */
+/* @var $certImage string */
+/* @var $pedagogSubject string | null */
+
+$qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $testAssignment->id], 'https')))
+    ->setMargin(5)
+    ->setSize(80);
+?>
+<div>
+    <div class="cert-page" style="background-image: url('./img/altyn-qyran-2025/cert.jpg'); background-size: cover; background-repeat: no-repeat; font-family: 'Arial'; height: 1200px">
+        <div style="padding-left: 400px; padding-top: 280px; text-align: right; width: 600px; height: 150px; font-size: 16px">
+            <div>
+                <?= $testAssignment->subject->name ?> пәнінен
+            </div>
+            <div>
+                қатысып, белсенділік танытқан үшін
+            </div>
+            <div style="font-family: 'Arial';">
+                <div style="font-weight: 500;">
+                    <?php
+                    if ($testAssignment->school !== null) {
+                        if ($testAssignment->school->city_id === 1 || $testAssignment->school->city_id === 2 || $testAssignment->school->city_id === 3) {
+                            echo $testAssignment->school->city->name;
+                        } else {
+                            echo $testAssignment->school->city->region->name . ', ' . $testAssignment->school->city->name;
+                        }
+                    } ?>
+                </div>
+                <div style="font-weight: 500; height: 50px">
+                    <?= $testAssignment->school->name ?>
+                </div>
+                <div style="margin-top: 20px; font-size: 18px">
+                    <?= $testAssignment->grade ?> сынып оқушысы
+                </div>
+                <div style="margin-top: 10px; font-weight: lighter; text-transform: uppercase;">
+                    <b><?= $testAssignment->surname . ' ' . $testAssignment->name . ' ' . $testAssignment->patronymic ?></b>
+                </div>
+            </div>
+        </div>
+        <div class="border" style="display: flex; padding-left: 140px; padding-top: 160px; color: #fff9f6">
+            <div id="cert-qrcode"><?= '<img src="' . $qrCode->writeDataUri() . '">' ?></div>
+            <div style="color: #0a0a0a; padding-top: 10px; font-size: 12px; padding-left: 0">
+                <div id="cert-number" style="color: #0a0a0a">Тіркеу №<?= $testAssignment->id ?></div>
+            </div>
+        </div>
+    </div>
+</div>
---
+++ frontend/views/olympiad/altyn-qyran-2025/_diploma.php
@@ -0,0 +1,76 @@
+<?php
+
+use common\models\Subject;
+use Da\QrCode\QrCode;
+
+/* @var $testAssignment \common\models\TestAssignment */
+/* @var $certImage string */
+/* @var $pedagogSubject string | null */
+
+$qrCode = (new QrCode(\yii\helpers\Url::toRoute(['olympiad/get-cert', 'id' => $testAssignment->id], 'https')))
+    ->setSize(90)->setMargin(5);
+
+$olympiadId = $testAssignment->olympiad_id;
+$cityId = $testAssignment->school->city_id;
+$regionId = $testAssignment->school->city->region_id;
+$template = 'diploma.jpg';
+if ($olympiadId === 28 && $cityId === 3) {
+    $template = "diploma-shymkent.jpg";
+} else if ($olympiadId === 28 && $regionId === 14) {
+    $template = "diploma-turkestan.jpg";
+}
+?>
+<div>
+    <div class="cert-page" style="background-image: url('./img/altyn-qyran-2025/<?= $template ?>'); background-size: cover; background-repeat: no-repeat; font-family: 'Arial'; height: 1200px">
+        <div style="font-size: 32px; padding-left: 20px; padding-top: 370px; text-transform: uppercase; color: red; text-align: center">
+            <b><?= $place ?></b>
+        </div>
+        <div style="padding-left: 100px; padding-top: 120px; text-align: center; width: 600px; height: 150px; font-size: 20px">
+            <div style="font-size: 16px">
+                <?= $testAssignment->subject->name ?> пәнінен
+                <br>
+                жеңімпаз болғаны үшін
+            </div>
+            <div style="padding-top: 15px; font-weight: lighter; text-transform: uppercase;">
+                <b><?= $testAssignment->surname . ' ' . $testAssignment->name . ' ' . $testAssignment->patronymic ?></b>
+            </div>
+            <div style="font-size: 16px; padding-top: 10px;">
+                <?= $testAssignment->grade ?> сынып оқушысы
+            </div>
+            <div style="padding-left: 20px; font-family: 'Arial'; height: 100px; width: 600px; font-size: 16px">
+                <div style="height: 90px">
+                    <div style="font-weight: 500;">
+                        <?php
+                        if ($testAssignment->school !== null) {
+                            if ($testAssignment->school->city_id === 1 || $testAssignment->school->city_id === 2 || $testAssignment->school->city_id === 3) {
+                                echo $testAssignment->school->city->name;
+                            } else {
+                                echo $testAssignment->school->city->region->name . ', ' . $testAssignment->school->city->name;
+                            }
+                        } ?>
+                    </div>
+                    <div style=" font-weight: 500;">
+                        <?= $testAssignment->school->name ?>
+                    </div>
+                </div>
+                <div style="padding-top: 10px;">
+                    <div style="text-transform: lowercase">
+                        Жетекшісі
+                    </div>
+                    <div style="margin-top: 5px; font-size: 20px; font-weight: lighter; text-transform: uppercase;">
+                        <b><?= $testAssignment->teacher_name ?></b>
+                    </div>
+                    <div style="text-transform: uppercase; margin-top: 10px">
+                        Марапатталады
+                    </div>
+                </div>
+            </div>
+        </div>
+        <div style="display: flex; padding-left: 40px; padding-top: 120px; color: #fff9f6">
+            <div id="cert-qrcode"><?= '<img src="' . $qrCode->writeDataUri() . '">' ?></div>
+            <div style="color: #0a0a0a; padding-top: 10px; font-size: 12px; padding-left: 0">
+                <div id="cert-number" style="color: #0a0a0a">Тіркеу №<?= $testAssignment->id ?></div>
+            </div>
+        </div>
+    </div>
+</div>
