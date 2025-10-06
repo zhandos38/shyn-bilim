3c48fc0de1bced1e49cfa67a7334d2da5fdff2ab
zhandos <zhandos38@gmail.com>
2025-10-06 21:35:14 +0500

    fixes

--- backend/views/kaspi-white-list/import.php
+++ backend/views/kaspi-white-list/import.php
@@ -4,7 +4,9 @@ use yii\helpers\Html;
 
 /** @var \backend\forms\ImportExcelWhiteList $model */
 
-$form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]); ?>
+$form = ActiveForm::begin(['options' => ['enctype' => 'multipart/form-data']]);
+$model->olympiad_id = 28;
+?>
 <?= $form->field($model, 'olympiad_id')->dropDownList(\yii\helpers\ArrayHelper::map(\common\models\Olympiad::find()->asArray()->all(), 'id', 'name'), ['prompt' => 'Выберите олимпиаду']) ?>
 <?= $form->field($model, 'file')->fileInput() ?>
 <?= Html::submitButton('Импорт', ['class' => 'btn btn-primary']) ?>
